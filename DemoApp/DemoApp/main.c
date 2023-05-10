c5afa9bc294df5e1b680c92124dc96a315b3173c
CodeRover22 <133115959+CodeRover22@users.noreply.github.com>
2023-05-10 17:20:23 +0530

    Add files via upload

---
+++ DemoApp/DemoApp/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  DemoApp
+//
+//  Created by MAC03 on 28/02/23.
+//
+
+#include <stdio.h> //1). header file dec
+
+int main(int argc, const char * argv[]) //2. Main function (entry point program)
+{
+    int a=23,b=45; //assignments
+    /*printf("Add:%d\n",a+b); //airthmetic
+    printf("Sub:%d\n",a-b);
+    printf("Mul:%d\n",a*b);
+    printf("Div:%d\n",a/b);*/
+    printf("Value of A:%d\n",a);
+    //a+=10; //a=a+10
+    //a-=10;
+    a*=10; //a=a*10
+    printf("Value of A:%d\n",a);
+    
+}
