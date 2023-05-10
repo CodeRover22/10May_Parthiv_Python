55b4263b2940ff1b2ec2837ad6dfed144ff922e7
CodeRover22 <133115959+CodeRover22@users.noreply.github.com>
2023-05-10 17:21:24 +0530

    Add files via upload

---
+++ If_else/If_else/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  If_else
+//
+//  Created by MAC03 on 02/03/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+    int a=93,b=45;
+    
+    if (a<b) //true
+    {
+        printf("A is min\n");
+    }
+    else //false
+    {
+        printf("A is max\n");
+    }
+}
